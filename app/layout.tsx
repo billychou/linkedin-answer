33d1ae8f4a1cb1ecc451140df399da9902030e5e
billychou <zsc1528@gmail.com>
2026-01-27 22:27:38 +0800

    feat: add Google Ads script to layout component
    - Include an asynchronous Google Ads script in the head of the layout component to enable ad functionality on the site.

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
+      <head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3410962713385660" crossOrigin="anonymous">
+     </script>
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background flex flex-col font-sans antialiased"
